2785fa13ef1e848f6618965bc83d8a8c039d949a
neon <aminal1g990@gmail.com>
2025-02-12 01:06:20 +0330

    create_model

--- CrmCore/urls.py
+++ CrmCore/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("CategoryManager/<int:category_id>",views.CategoryManager.as_view()),
     path("CategoryManager",views.CategoryManager.as_view()),
@@ -30,7 +31,7 @@ urlpatterns = [
     
     
     
-]
+]    +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
     
