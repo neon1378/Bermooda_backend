02842efe3efe2b8f67ad568fdbba50577c877df8
neon <aminal1g990@gmail.com>
2025-02-12 17:40:24 +0330

    add workspace active

--- CrmCore/views.py
+++ CrmCore/views.py
@@ -869,11 +869,28 @@ class CustomerUserView(APIView):
             })
         group_crm_id = request.GET.get("group_crm_id",None)
         custommer_objs = CustomerUser.objects.filter(group_crm_id=group_crm_id)
-        serializer_data = CustomerSmallSerializer(custommer_objs,many=True)
+        data_list = []
+        for custommer_obj in custommer_objs:
+            not_exsit =True
+
+            for data in data_list:
+                if data['label_id'] == custommer_obj.label.id:
+                    data['customer_list'].append(serializer_data = CustomerSmallSerializer(custommer_obj))
+                    not_exsit=False
+                    break
+            if not_exsit:
+                data_list.append({
+                    "label_id":custommer_obj.label.id,
+                    "color":custommer_obj.label.color,
+                    "title":custommer_obj.label.title
+                })
+
+
+        # serializer_data = CustomerSmallSerializer(custommer_objs,many=True)
         return Response(status=status.HTTP_200_OK, data={
             "status": True,
-            "data": "success",
-            "data": serializer_data.data
+            "message": "success",
+            "data": data_list
         })
 
     def post(self,request):
