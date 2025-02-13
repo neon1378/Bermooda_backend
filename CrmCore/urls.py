00acf09443072da0a5788ef299a07fb5ffd9d13c
neon <aminal1g990@gmail.com>
2025-02-13 15:03:11 +0330

    fix project ws

--- CrmCore/urls.py
+++ CrmCore/urls.py
@@ -30,9 +30,8 @@ urlpatterns = [
     path("ExistMemberCrm/<int:group_id>",views.exist_member_in_crm),
     path("CustomerUserView", views.CustomerUserView.as_view()),
     path("CampaignManager", views.CampaignManager.as_view()),
+    path("CampaignManager/<int:id>", views.CampaignManager.as_view()),
 
-
-    
 ]
 
 
