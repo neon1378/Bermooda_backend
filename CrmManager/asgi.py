e1a899772ba11579a8bd9ae01a4e5046fed777a5
neon <aminal1g990@gmail.com>
2025-02-20 09:43:01 +0330

    neon

--- CrmManager/asgi.py
+++ CrmManager/asgi.py
@@ -5,11 +5,12 @@ from channels.auth import AuthMiddlewareStack
 import django
 import ProjectManager.routing as project_manager
 import SupportManager.routing as SuportManager
+import CrmCore.routing  as CrmManager
 from .middleware import TokenAuthMiddleware,JWTAuthMiddleware
 from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrmManager.settings')
-routers = SuportManager.websocket_urlpatterns + project_manager.websocket_urlpatterns
+routers = SuportManager.websocket_urlpatterns + project_manager.websocket_urlpatterns + CrmManager.websocket_urlpatterns
 
 
 application = ProtocolTypeRouter({
