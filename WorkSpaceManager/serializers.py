a9190e0fc518c0942e5276b9ba8913a0618dca32
neon <aminal1g990@gmail.com>
2025-02-22 01:47:10 +0330

    neon

--- WorkSpaceManager/serializers.py
+++ WorkSpaceManager/serializers.py
@@ -40,14 +40,16 @@ class WorkSpaceSerializer(ModelSerializer):
         
         jadoo_brand_name= validated_data.get("jadoo_brand_name",None)
         if jadoo_brand_name == None:
-            raise serializers.ValidationError({"error":{
-                "detail":"نام پروفایل اجباری میباشد"
-            }})
-        if WorkSpace.objects.filter(jadoo_brand_name=jadoo_brand_name).exists():
+            raise serializers.ValidationError({
+                "status":False,
+                "message":"نام کاربری اجباری میباشد"
+            })
+        if WorkSpace.all_objects.filter(jadoo_brand_name=jadoo_brand_name).exists():
             if instance.jadoo_brand_name != jadoo_brand_name:
-                raise serializers.ValidationError({"error":{
-                "detail":"نام پروفایل انتخاب شده درحال حاضر وجود دارد"
-                }})
+                raise serializers.ValidationError({
+                    "status":False,
+                    "message":"نام کاربری وارد شده در حال حاضر وجود دارد"
+                })
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         
