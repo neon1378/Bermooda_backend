539277283239cf4c0fc8ef5c2f9acc3841b30d48
neon <aminal1g990@gmail.com>
2025-03-01 20:11:20 +0330

    neon

--- ProjectManager/routing.py
+++ ProjectManager/routing.py
@@ -3,7 +3,7 @@ from . import consumers
 websocket_urlpatterns = [
 
     path("ws/ProjectChatWs/<project_id>",consumers.ProjectChatWs.as_asgi()),
-    path("ws/ProjectTask/<project_id>",consumers.ProjectTask.as_asgi()),
+    path("ws/ProjectTask/<project_id>",consumers.ProjectTaskConsumer.as_asgi()),
 
     path("ws/Project/GroupMessage/<project_id>",consumers.ProjectChatMainWs.as_asgi()),
     # path("ws/test/ProjectTask/<project_id>",consumers.ProjectTaskConsumer.as_asgi()),
