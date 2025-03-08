a2eed33c995979b70f82d35521f24bc80024bb1e
neon <aminal1g990@gmail.com>
2025-03-08 13:10:07 +0330

    neon

--- CustomerFinance/urls.py
+++ CustomerFinance/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
 
+
+router = DefaultRouter()
+router.register(r'InvoiceStatusManager', views.InvoiceStatusManager, basename='invoice-status')
 urlpatterns = [
     path("InvoiceManager",views.InvoiceManager.as_view()),
     path("InvoiceManager/<int:invoice_id>",views.InvoiceManager.as_view()),
-
-    path("InvoiceStatusManager",views.InvoiceStatusManager.as_view()),
-    path("InvoiceStatusManager/<int:status_id>",views.InvoiceStatusManager.as_view()),
+    path('', include(router.urls)),
+    # path("InvoiceStatusManager",views.InvoiceStatusManager.as_view()),
+    # path("InvoiceStatusManager/<int:status_id>",views.InvoiceStatusManager.as_view()),
 ]
