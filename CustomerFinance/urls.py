775a8b20fd330ef603c1c1fb07613c002b3dcdd6
neon <aminal1g990@gmail.com>
2025-03-09 17:50:04 +0330

    neon

--- CustomerFinance/urls.py
+++ CustomerFinance/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     # path('', include(router.urls)),
     path("InvoiceStatusManager",views.InvoiceStatusManager.as_view()),
     path("InvoiceStatusManager/<int:status_id>",views.InvoiceStatusDetailManager.as_view()),
-    path("ChangeInvoiceStatus/<int:status_id>",views.change_invoice_status),
+    path("ChangeInvoiceStatus/<int:invoice_id>",views.change_invoice_status),
 ]
