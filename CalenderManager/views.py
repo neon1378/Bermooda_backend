b5c68c046cf03807e00fe12e2de735d29eeef0cd
neon <aminal1g990@gmail.com>
2025-03-17 13:07:58 +0330

    neon

--- CalenderManager/views.py
+++ CalenderManager/views.py
@@ -89,11 +89,12 @@ class CalenderManger(APIView):
 
         for day in range(1, num_days + 1):
             g_date = date(year, month, day)  # Create Gregorian date
-
+            print(g_date)
             # Filter only checklists for this specific day
             check_list_items =[]
             for item in check_list_objs:
                 if item.date_time_to_start_main:
+                    print (item.date_time_to_start_main.date())
                     if item.date_time_to_start_main.date() == g_date:
                         check_list_items.append(item)
 
