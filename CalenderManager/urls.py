41f6e41fbb75c1bb1d9c91a1e991367aa382bbd5
neon <neon.313.101@gmail.com>
2025-04-16 05:11:24 -0300

    neon

--- CalenderManager/urls.py
+++ CalenderManager/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import  views
 urlpatterns = [
+
     path("Calender",views.CalenderManger.as_view()),
+    path("Calender/<int:meeting_id>", views.CalenderManger.as_view()),
+
     path("MeetingLabelManager",views.MeetingLabelManager.as_view()),
     path("MeetingLabelManager/<int:label_id>",views.MeetingLabelManager.as_view()),
     path("GetAMeeting/<int:meeting_id>", views.get_a_meeting),
