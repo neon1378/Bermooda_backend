a1138efbdb406141babacbc7596d98824358b960
neon <neon.313.101@gmail.com>
2025-04-22 08:13:07 -0300

    neon

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ from UserManager.models import UserAccount
 user_objs = UserAccount.objects.all()
 import requests
 for user_acc in user_objs:
-
+    if user_acc.phone_number:
         jadoo_base_url = os.getenv("JADOO_BASE_URL")
         # send user to jadoo
 
         url = f"{jadoo_base_url}/user/auth/createBusinessUser"
+
         payload = {
             "mobile": user_acc.phone_number,
             "fullname": user_acc.fullname,
