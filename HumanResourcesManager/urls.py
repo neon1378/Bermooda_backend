f797156f255616fde0b1051ef67688eaf55cfce4
neon <neon.313.101@gmail.com>
2025-04-26 07:31:38 -0300

    neon

--- HumanResourcesManager/urls.py
+++ HumanResourcesManager/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path("FolderManager",views.FolderManager.as_view()),
+    path("FolderManager/<str:slug>", views.FolderManager.as_view()),
+
 ]
