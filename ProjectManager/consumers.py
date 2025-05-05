60828f04999a8da93e301b0dfe4a667e1f879283
neon <neon.313.101@gmail.com>
2025-05-05 07:19:56 -0300

    neon

--- ProjectManager/consumers.py
+++ ProjectManager/consumers.py
@@ -28,230 +28,6 @@ load_dotenv()
 
 
 
-class ProjectChatWs(WebsocketConsumer):
-    def connect(self):
-        self.user = self.scope['user']
-
-        if self.user.is_authenticated:
-            self.accept()
-        else:
-            self.close(code=1)
-        self.project_id = self.scope['url_route']['kwargs']['project_id']
-        self.project_obj = Project.objects.get(id=self.project_id)
-        async_to_sync(self.channel_layer.group_add)(
-            f"{self.project_id}_chat",self.channel_name
-        )
-
-    def receive(self, text_data=None, bytes_data=None):
-        base_url = os.getenv("BASE_URL")
-        data = json.loads(text_data)
-
-        if data['request_type'] == "create_message":
-            message = data.get("message",None)
-
-            
-
-            new_project_chat = ProjectChat(creator=self.user,body=message,project=self.project_obj)
-            try:
-                voice_obj = MainFile.objects.get(id=data['voice'])
-                new_project_chat.voice_file = voice_obj
-                voice_obj.its_blong=True
-                voice_obj.save()
-            except:
-                pass
-            try:
-                file_obj = MainFile.objects.get(id=data['file'])
-                new_project_chat.main_file =file_obj
-                file_obj.its_blong=True
-                file_obj.save()
-            except:pass
-            # new_project_chat.project=new_project_chat
-            new_project_chat.save()
-            
-            
-            token = Token.objects.get_or_create(user=new_project_chat.creator)[0]
-        
-            event = {
-                "type":"chat_handler",
-                "message":message,
-                "user_id":self.user.id,
-                "new_project_chat_id":new_project_chat.id,
-                "created":new_project_chat.jcreated()
-                
-            }
-            
-            async_to_sync(self.channel_layer.group_send)(
-                f"{self.project_id}_chat",
-                event
-            )
-        elif data['request_type'] == "read_message":
-
-            messages = ProjectChat.objects.filter(project=self.project_obj).order_by("-id")
-            message_list = []
-            for message in messages:
-                message_dic = {
-                    "id":message.id,
-                    "body":message.body,
-                    "created":message.jcreated()
-                    
-                }
-                if message.main_file:
-                    message_dic['file'] = f"{base_url}{message.main_file.file.url}"
-                if message.voice_file:
-                    message_dic['voice'] = f"{base_url}{message.voice_file.file.url}"
-
-                if message.creator.id == self.user.id:
-                    message_dic['creator'] = "self"
-                else:
-                    message_dic['creator'] = "other"
-                message_list.append(message_dic)
-            self.send(json.dumps(
-                message_list
-            ))
-
-    def chat_handler(self,event):
-        base_url = os.getenv("BASE_URL")
-        project_chat_obj = ProjectChat.objects.get(id=event['new_project_chat_id'])
-        token = Token.objects.get_or_create(user=project_chat_obj.creator)[0]
-        user_obj = UserAccount.objects.get(id=event['user_id'])
-        message_data = {"message":{"body":event['message'], "created":event['created'],"fullname":user_obj.fullname}}
-        if self.user.id == event['user_id'] :
-            message_data['message']['creator'] = "self"
-        else:
-            message_data['message']['creator'] = "other"
-        if project_chat_obj.main_file:
-            message_data['message']['file'] = f"{base_url}{project_chat_obj.main_file.url}"
-        if project_chat_obj.voice_file:
-            message_data['message']['voice']  = f"{base_url}{project_chat_obj.voice_file.url}"
-        self.send(json.dumps(
-            message_data
-        ))
-
-    def disconnect(self,code=None):
-        
-        async_to_sync(self.channel_layer.group_discard)(
-            f"{self.project_id}_chat",
-            self.channel_name
-        )
-        self.close(code=0)
-
-
-
-class ProjectChatMainWs(WebsocketConsumer):
-    def paginate_queryset(self,page_number,queryset):
-
-
-        # Set up custom pagination
-        paginator = Paginator(queryset.order_by("-id"), 20)  # Set items per page
-
-        # Check if the requested page exists
-        if page_number > paginator.num_pages:
-            return {
-                "count": paginator.count,
-                "next": None,
-                "previous": None,
-                "data": []
-            }
-
-        # Get the page
-        page = paginator.get_page(page_number)
-        serializer_data=ProjectChatSerializer(page.object_list,many=True)
-        for item in serializer_data.data:
-            item['creator']['self'] = item['creator']['id'] == self.user.id
-        return {
-            "count": paginator.count,
-            "next": page.next_page_number() if page.has_next() else None,
-            "previous": page.previous_page_number() if page.has_previous() else None,
-            "list": serializer_data.data
-        }
-
-    def connect(self):
-        self.user = self.scope['user']
-
-        if self.user.is_authenticated:
-            self.accept()
-        else:
-            self.close(code=1)
-        self.project_id = self.scope['url_route']['kwargs']['project_id']
-        self.project_obj = Project.objects.get(id=self.project_id)
-        async_to_sync(self.channel_layer.group_add)(
-            f"{self.project_id}_main",self.channel_name
-        )
-
-    def receive(self, text_data=None, bytes_data=None):
-        base_url = os.getenv("BASE_URL")
-        data = json.loads(text_data)
-
-
-        if data['command'] == "create_message":
-            data['data']['creator_id'] = self.user.id
-            data['data']['project_id'] = self.project_obj.id
-
-            
-
-            serializer_data = ProjectChatSerializer(data=data['data'])
-            if serializer_data.is_valid():
-                new_project_chat = serializer_data.save()
-
-
-          
-            
-          
-        
-                event = {
-                    "type":"chat_handler",
-                    "chat_id":new_project_chat.id,
-                    "creator_id" :self.user.id
-                   
-                
-
-                    
-                }
-                async_to_sync(self.channel_layer.group_send)(
-                    f"{self.project_id}_main",
-                    event
-                )
-            else:
-                self.send(json.dumps(
-                    serializer_data.errors
-                ))
-         
-        elif data['command'] == "message_list":
-            try:
-                page = data['data']['page']
-            except:
-                page = 1
-            project_chats = ProjectChat.objects.filter(project=self.project_obj).order_by("-id")
-            data = self.paginate_queryset(page_number=page,queryset=project_chats)
-
-            self.send(json.dumps(
-                {
-                    "data_type":"message_list",
-                    "data":data
-                }
-            ))
-
-    def chat_handler(self,event):
-        project_chat_obj = ProjectChat.objects.get(id=event['chat_id'])
-
-        serializer_data= ProjectChatSerializer(project_chat_obj)
-        serializer_data.data['creator']['self'] = serializer_data.data['creator']['id'] == self.user.id
-        self.send(json.dumps(
-            {
-                "data_type":"create_message",
-                "data":serializer_data.data,
-
-
-            }
-        ))
-
-    def disconnect(self,code=None):
-        
-        async_to_sync(self.channel_layer.group_discard)(
-            f"{self.project_id}_main",
-            self.channel_name
-        )
-        self.close(code=0)
 
 
 
@@ -394,14 +170,7 @@ class ProjectTask(AsyncWebsocketConsumer):
 
         })
 
-    # @sync_to_async
-    # def destroy_a_message(self,data):
-    #     main_data= data['data']
-    #     message_obj = get_object_or_404(ProjectMessage,id=main_data['message_id'])
-    #     message_obj.delete()
-    #
-    #
-    #
+
     @sync_to_async
     def _create_a_message(self,data):
         main_data = data['data']
@@ -515,12 +284,19 @@ class ProjectTask(AsyncWebsocketConsumer):
             Prefetch('check_list',
                      queryset=CheckList.objects.select_related('responsible_for_doing'))
         )
-
+        print(self._has_admin_access())
         if self._has_admin_access():
             return base_qs
-        return [task for task in base_qs
-                if any(check.responsible_for_doing == self.user
-                       for check in task.check_list.all())]
+        task_list = []
+        for task in base_qs:
+            for check_list in task.check_list.all():
+                if check_list.responsible_for_doing == self.user:
+                    task_list.append(task)
+                    break
+        return task_list
+        # return [task for task in base_qs
+        #         if any(check.responsible_for_doing == self.user
+        #                for check in task.check_list.all())]
     @sync_to_async
     def _has_admin_access(self):
 
@@ -541,11 +317,7 @@ class ProjectTask(AsyncWebsocketConsumer):
             for permission in member.permissions.all():
                 if permission.permission_name == "project board":
                     return  permission.permission_type
-            # return next(
-            #     (p.permission_type for p in member.permissions.all()
-            #      if p.permission_name == "project board"),
-            #     None
-            # )
+
         except ObjectDoesNotExist:
             return None
 
