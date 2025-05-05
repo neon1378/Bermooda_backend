1c54ad80825e1896ac5b45b46b90389ec916787b
neon <neon.313.101@gmail.com>
2025-05-05 06:19:36 -0300

    neon

--- ProjectManager/admin.py
+++ ProjectManager/admin.py
@@ -114,3 +114,18 @@ class TaskReportAdmin(admin.ModelAdmin):
         self.message_user(request, f'{queryset.count()} items restored successfully.')
 
 
+@admin.register(ProjectMessage)
+class CategoryProjectAdmin(admin.ModelAdmin):
+    list_display = ( 'is_deleted', 'deleted_at',"id")
+    list_filter = ('is_deleted',)
+    actions = ['restore_selected']
+
+    def get_queryset(self, request):
+        # Use the custom manager's queryset
+        return self.model.all_objects.get_queryset()
+
+    @admin.action(description='Restore selected items')
+    def restore_selected(self, request, queryset):
+        # Restore soft-deleted items
+        queryset.update(is_deleted=False, deleted_at=None)
+        self.message_user(request, f'{queryset.count()} items restored successfully.')
