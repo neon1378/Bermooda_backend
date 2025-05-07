288bc1081695d48ff33e1a4011c83ebd7f5f9dbd
neon <aminal1g990@gmail.com>
2025-05-07 12:48:59 +0330

    neon

--- core/routing.py
+++ core/routing.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import consumers
 websocket_urlpatterns = [
     path("ws/UploadProgressConsumer/<upload_id>", consumers.UploadProgressConsumer.as_asgi()),
-    path("ws/CoreBermooda/", consumers.CoreWebSocket.as_asgi()),
+    path("ws/CoreBermooda", consumers.CoreWebSocket.as_asgi()),
 
 ]
