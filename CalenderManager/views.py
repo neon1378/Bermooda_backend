c292287d8e336cffae054d6eae6353bbbef46c8e
OMEN 15 <neon.313.101@gmail.com>
2025-05-17 08:02:45 -0700

    change_workspace_id

--- CalenderManager/views.py
+++ CalenderManager/views.py
@@ -80,7 +80,7 @@ class MeetingLabelManager(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CalenderManger(APIView):
-    permission_classes = [IsAuthenticated,IsWorkSpaceMemberAccess]
+    permission_classes = [IsAuthenticated]
 
     def is_jalali_leap_year(self, year):
         """
