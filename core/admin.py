5fc7d6a09e70bf398bcaba6f49beaeb38271e615
OMEN 15 <neon.313.101@gmail.com>
2025-05-22 00:45:46 -0700

    neon

--- core/admin.py
+++ core/admin.py
@@ -9,3 +9,18 @@ admin.site.register(State)
 admin.site.register(MainFile)
 admin.site.register(Reminder)
 
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ( 'is_deleted', 'deleted_at',"name","id")
+    list_filter = ('is_deleted',)
+    actions = ['restore_selected']
+
+    def get_queryset(self, request):
+        # Use the custom manager's queryset
+        return self.model.all_objects.get_queryset()
+
+    @admin.action(description='Restore selected items')
+    def restore_selected(self, request, queryset):
+        # Restore soft-deleted items
+        queryset.update(is_deleted=False, deleted_at=None)
+        self.message_user(request, f'{queryset.count()} items restored successfully.')
