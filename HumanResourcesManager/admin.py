08224577eff07aa488102720a0f701a2f507696e
OMEN 15 <neon.313.101@gmail.com>
2025-05-24 03:27:29 -0700

    neon

--- HumanResourcesManager/admin.py
+++ HumanResourcesManager/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 from .models import  *
 # Register your models here.
 @admin.register(Folder)
-class IndustrialActivityAdmin(admin.ModelAdmin):
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ( 'is_deleted', 'deleted_at',"id")
+    list_filter = ('is_deleted',)
+    actions = ['restore_selected']
+
+    def get_queryset(self, request):
+        # Use the custom manager's queryset
+        return self.model.all_objects.get_queryset()
+
+    @admin.action(description='Restore selected items')
+    def restore_selected(self, request, queryset):
+        # Restore soft-deleted items
+        queryset.update(is_deleted=False, deleted_at=None)
+        self.message_user(request, f'{queryset.count()} items restored successfully.')
+
+
+
+@admin.register(EmployeeRequest)
+class EmployeeRequestAdmin(admin.ModelAdmin):
     list_display = ( 'is_deleted', 'deleted_at',"id")
     list_filter = ('is_deleted',)
     actions = ['restore_selected']
